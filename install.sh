8b9a4820ea0133c0a6e3212f2ccbad0e9eb29092
MrParano1d <jps@marla.one>
2022-08-04 21:26:38 +0200

    fix(install): :sparkles: added dependency checks

--- install.sh
+++ install.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 DOTFILE_PATH=$PWD
 
-echo "Installing yay"
-cd /tmp/ && sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+if ! command -v yay &> /dev/null
+then
+    echo "Installing yay"
+    cd /tmp/ && sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+    exit
+fi
 
-echo "Installing neovim"
-yay -S nvim ripgrep
+if ! command -v nvim &> /dev/null
+then
+    echo "Installing nvim"
+    yay -S neovim
+    exit
+fi
 
-echo "Copy .dotfiles"
+if ! command -v rg &> /dev/null
+then
+    echo "Installing ripgrep"
+    yay -S ripgrep
+    exit
+fi
+
+echo "Copying .dotfiles"
 cd $DOTFILE_PATH
 cp -r ./.config/* ~/.config/
 
