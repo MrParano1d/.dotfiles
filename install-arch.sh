904ff81bf9c29ae988fa768abad18b79d115f704
Jan-Philipp Schmeißer <jps@easy-m.de>
2022-08-05 08:15:28 +0200

    fix(install): :fix: removed exit after package install
    added unzip as new dependency

--- install-arch.sh
+++ install-arch.sh
@@ -3,26 +3,30 @@ DOTFILE_PATH=$PWD
 
 if ! command -v yay &> /dev/null
 then
-    echo "Installing yay"
+    echo "=== Installing yay ==="
     cd /tmp/ && sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-    exit
 fi
 
 if ! command -v nvim &> /dev/null
 then
-    echo "Installing nvim"
+    echo "=== Installing nvim ==="
     yay -S neovim
-    exit
 fi
 
 if ! command -v rg &> /dev/null
 then
-    echo "Installing ripgrep"
+    echo "=== Installing ripgrep ==="
     yay -S ripgrep
-    exit
 fi
 
-echo "Copying .dotfiles"
+if ! command -v unzip &> /dev/null
+then
+  echo "=== Installing unzip ==="
+  yay -S unzip
+fi
+
+echo "=== Copying .dotfiles ==="
 cd $DOTFILE_PATH
 cp -r ./.config/* ~/.config/
 
+echo "=== DONE ==="
